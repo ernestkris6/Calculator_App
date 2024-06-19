933a6f5f2d827b1f9ff8da0a736cc05d0a2f8d4d
ERNEST CHRIS <ernestchris6@gmail.com>
2024-06-19 14:30:12 +0100

    star rating

---
+++ src/StarRating.js
@@ -0,0 +1,7 @@
+export default function StarRating(){
+    return(
+        <div>
+            <h1>Hello World!!!</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+// import App from './App';
+// import './index.css'
 
-
+import StarRating from './StarRating';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <StrictMode>
-        <App />
+        {/*<App />*/}
+        <StarRating />
     </StrictMode>
-)
+);   
