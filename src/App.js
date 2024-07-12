fcfd323b5229729f38b440f93b41f8a647baed91
ERNEST CHRIS <ernestchris6@gmail.com>
2024-07-12 17:41:17 +0100

    useRefs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarRating";
 
 
@@ -173,12 +173,31 @@ function Logo() {
 }
 
 function Search({query, setQuery}) {
+  
+  const inputEl = useRef(null);
 
   useEffect(function(){
-    const el = document.querySelector('.search');
-    console.log(el);
-    el.focus();
-  }, [])
+    //console.log(inputEl.current);
+
+    function callback(e){
+      if(e.code === 'Enter'){
+        if(document.activeElement === inputEl.current) return;
+
+        inputEl.current.focus();
+        setQuery("")
+      }
+ }
+
+      document.addEventListener('keydown', callback)
+
+      return () => document.addEventListener('keydown', callback)
+
+  }, [setQuery])
+  // useEffect(function(){
+  //   const el = document.querySelector('.search');
+  //   console.log(el);
+  //   el.focus();
+  // }, [])
 
   return (
     <input
@@ -187,6 +206,7 @@ function Search({query, setQuery}) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
